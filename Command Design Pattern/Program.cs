c74df0d2bc13e449a365ecb4cbd9a9145f4f1f21
jd_786 <jaydeep_shil@certree.com>
2022-12-01 13:49:07 +0530

    Add project files.

---
+++ Command Design Pattern/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Command_Design_Pattern
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Product product = new Product();
+      ICommand command = new RedoCommand(product);
+      Invoker invoker = new Invoker(command);
+      invoker.ExecuteCommand();
+    }
+  }
+
+  public interface ICommand
+  {
+    void Do();
+  }
+
+  public class RedoCommand : ICommand
+  {
+    private Product Product;
+    public RedoCommand(Product product) { this.Product = product; }
+    public void Do() { Product.Redo(); }
+  }
+
+  public class UndoCommand : ICommand
+  {
+    private Product Product;
+    public UndoCommand(Product product) { this.Product = product; }
+    public void Do() { Product.Undo(); }
+  }
+
+  public class Product
+  {
+    public void Redo()
+    {
+      Console.WriteLine("Redo done!");
+    }
+
+    public void Undo()
+    {
+      Console.WriteLine("Undo done!");
+    }
+  }
+
+  public class Invoker
+  {
+    private ICommand command;
+    public Invoker(ICommand command)
+    {
+      this.command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+      command.Do();
+    }
+  }
+}
